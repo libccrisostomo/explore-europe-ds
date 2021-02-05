1fedf3b3fd8b843b99d9a5520edcecc151ebe06e
libccrisostomo <55488494+libccrisostomo@users.noreply.github.com>
2021-02-05 16:09:47 +0000

    Updated collecting_data and run FINAL_VERSION

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from collect_data import scrape_LI_page
 
-scrape_LI_page(username='laura.ibcc98@gmail.com',
-               password='helloworld',
+scrape_LI_page(username='enter username',
+               password='enter password',
                keyword='Data Scientist',
                location=['European Union'],
                experience_levels=['Associate', 'Entry level', 'Internship'])
