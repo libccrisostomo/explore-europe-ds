e5cf3eeb0858fbb4c8a43659840779922e25e814
libccrisostomo <55488494+libccrisostomo@users.noreply.github.com>
2021-02-08 14:11:47 +0000

    Updated visualize_data.py

--- visualize_data.py
+++ visualize_data.py
@@ -17,10 +17,10 @@ for filename in os.listdir(origin_directory):
     elif filename[-7:-5] != 'EU':  # this is for my personalized sample
         df_joined_locations = join_locations(df_joined_locations, df)
     plot_sunburst(df, show=True, save=False, location=filename[filename.rfind('_')+1:filename.rfind('.')],
-                  template='plotly_dark', transparent_bg=False)
+                  template='plotly', transparent_bg=False)
 
 # Plot with average salary, cost of living, and number of jobs per city
-plot_scatter(df_joined_locations, show=True, save=False, min_jobs=25, text=True, template='plotly_dark',
+plot_scatter(df_joined_locations, show=True, save=False, min_jobs=25, text=True, template='plotly',
              transparent_bg=False)
 
 del df, df_joined_locations, origin_directory, filename
